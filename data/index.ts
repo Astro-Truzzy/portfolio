040e96ac77ad4116edc3e39cd1aa00746d1ec5b4
Astro-Truzzy <taresy.dev@gmail.com>
2025-02-25 16:37:05 +0100

    change an item in index.ts

--- data/index.ts
+++ data/index.ts
@@ -119,7 +119,7 @@ export const navItems = [
         },
         {
           quote:
-            "Collaborating with Trust was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Trust's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+            "Collaborating with Trust was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Trust's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Trust is the ideal partner.",
           name: "Esosa John",
           title: "Founder of Esosa John Foundation (EJF)",
         },
